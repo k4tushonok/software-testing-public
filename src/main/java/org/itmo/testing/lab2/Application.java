b0947b3afa457931dacf114181ca9b7980b22e2a
k4tushonok <83214904+k4tushonok@users.noreply.github.com>
2025-03-31 20:22:29 +0000

    Deploying to gh-pages from @ k4tushonok/software-testing-public@ed9528a2030bb75de1ae08094f3696e310d18cec 🚀

--- src/main/java/org/itmo/testing/lab2/Application.java
+++
@@ -1,9 +0,0 @@
-package org.itmo.testing.lab2;
-
-import org.itmo.testing.lab2.controller.UserAnalyticsController;
-
-public class Application {
-    public static void main(String[] args) {
-        UserAnalyticsController.createApp().start(7000);
-    }
-}
